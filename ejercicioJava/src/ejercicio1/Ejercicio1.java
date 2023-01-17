c5f366421b5aa2c16ab0cb44512ca5db1834a428
Dani <danielporras147@outlook.es>
2023-01-17 19:08:27 +0100

    ejercicio1

---
+++ ejercicioJava/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,21 @@
+//1. Crea un programa que diga el resultado de sumar 36 y 27
+
+package ejercicio1;
+
+public class Ejercicio1 {
+
+	public static void sumaNumeros() {
+
+		int num1 = 36;
+		int num2 = 27;
+		int suma = num1 + num2;
+
+		System.out.println("El resultado de sumar " + num1 + " + " + num2 + " es: " + suma);
+	}
+
+	public static void main(String[] args) {
+		sumaNumeros();
+
+	}
+
+}
