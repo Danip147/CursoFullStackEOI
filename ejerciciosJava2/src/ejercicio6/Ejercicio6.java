0e8c855f4a9776dcc565bfa0f4807bf816fa8c1c
Dani <danielporras147@outlook.es>
2023-01-19 19:44:32 +0100

    Ejercicio 7

--- ejerciciosJava2/src/ejercicio6/Ejercicio6.java
+++ ejerciciosJava2/src/ejercicio6/Ejercicio6.java
@@ -10,10 +10,10 @@ public class Ejercicio6 {
 
 	public static void dimeNumMayor() {
 		Scanner sc = new Scanner(System.in);
-		//Pedimos datos usuario
+		// Pedimos datos usuario
 		System.out.println("Introduzca primer número");
 		int num1 = sc.nextInt();
-		sc.nextLine(); //Limpiar Buffer
+		sc.nextLine(); // Limpiar Buffer
 
 		System.out.println("Introduzca segundo número");
 		int num2 = sc.nextInt();
@@ -21,8 +21,8 @@ public class Ejercicio6 {
 		System.out.println("Introduzca tercer número");
 		int num3 = sc.nextInt();
 		sc.nextLine();
-		
-		//Condicional
+
+		// Condicional
 		if ((num1 > num2) && (num1 > num3)) {
 			System.out.println(num1 + " es el mayor de los 3");
 		} else if ((num2 > num1) && (num2 > num3)) {
@@ -30,7 +30,7 @@ public class Ejercicio6 {
 		} else {
 			System.out.println(num3 + " es el mayor de los 3");
 		}
-		
+
 		sc.close();
 	}
 
---
+++ ejerciciosJava2/src/ejercicio7/Ejercicio7.java
@@ -0,0 +1,52 @@
+/*
+ * 7. Programa que pida al usuario una cantidad de horas, de minutos y de segundos (3 números diferentes).
+ *    Comprueba que la hora está en el rango de 0 a 23, los minutos y los segundos de 0 a 59. 
+ *    
+ *   Si todo es correcto, muestra al usuario la hora en formato HH:MM:SS (2 cifras para cada parte 
+ *   rellenando con 0 a la izquierda si es una cifra), y si no, muestra un mensaje de error. Pista: Repasa la 
+ *   parte de formatear la salida de texto para números enteros
+ */
+
+
+package ejercicio7;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+	
+	public static void dimeHoras() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un número de horas: ");
+		int horas = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Introduce un número de minutos: ");
+		int minutos = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Introduce un número de segundos: ");
+		int segundos = sc.nextInt();
+		sc.nextLine();
+		
+		if (horas > 0 && horas <= 24) {
+			if (minutos > 0 && minutos <= 59) {
+				if (segundos > 0 && segundos <= 59) {
+					System.out.printf("Hora introducida: %02d:%02d:%02d", horas, minutos, segundos);
+				} else {
+					System.out.println("Error al introducir el tiempo");
+				}
+			} else {
+				System.out.println("Error al introducir el tiempo");
+			}
+		} else {
+			System.out.println("Error al introducir el tiempo");
+		}
+		
+		
+	sc.close();
+	}
+
+	public static void main(String[] args) {
+		dimeHoras();
+
+	}
+
+}
