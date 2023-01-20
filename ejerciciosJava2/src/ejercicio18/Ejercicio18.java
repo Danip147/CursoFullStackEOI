c56b93f704c092deeefe2c148d042ff700a7aad6
Dani <danielporras147@outlook.es>
2023-01-20 19:20:55 +0100

    Ejercicio 18

---
+++ ejerciciosJava2/src/ejercicio18/Ejercicio18.java
@@ -0,0 +1,22 @@
+/**
+ * 18. Muestra de 5 en 5, los números del 0 al 100.
+ */
+
+package ejercicio18;
+
+public class Ejercicio18 {
+	
+	public static void ejercicio18() {
+	
+		for (int i = 0; i <= 100; i+= 5) {
+			System.out.println(i);
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		ejercicio18();
+
+	}
+
+}
