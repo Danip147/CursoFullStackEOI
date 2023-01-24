3a079532d84dfdcfc0150bf2342b27afbd98f379
Dani <danielporras147@outlook.es>
2023-01-24 11:57:22 +0100

    Ejercicio 21

--- ejerciciosJava2/src/ejercicio21/Ejercicio21.java
+++ ejerciciosJava2/src/ejercicio21/Ejercicio21.java
@@ -17,7 +17,7 @@ public class Ejercicio21 {
 
 		for (int i = 1; i <= numUsuario; i++) {
 			if (numUsuario % i == 0) {
-				System.out.println("Los números divisores de " + numUsuario+ " son: " +i);
+				System.out.println("Los números divisores de " + numUsuario + " son: " + i);
 			}
 		}
 
@@ -25,8 +25,53 @@ public class Ejercicio21 {
 
 	}
 
+	public static void ejercicio21b() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un número: ");
+		int numUsuario = Integer.parseInt(sc.nextLine());
+
+		int i = 1;
+
+		while (i <= numUsuario) {
+			if (numUsuario % i == 0) {
+				System.out.println("Los números divisores de " + numUsuario + " son: " + i);
+			}
+			i++;
+		}
+
+		sc.close();
+
+	}
+
+	/**
+	 * Decir si un número es primo o no Primo es si solamente es divisible por el 1
+	 * y por el mismo (ej:2,3,5,7,13,17...)
+	 */
+	public static void ejercicio21Primos() {
+		int contador = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un número: ");
+		int numUsuario = sc.nextInt();
+		sc.nextLine(); // Limpia buffer
+
+		for (int i = 1; i <= numUsuario; i++) {
+			if (numUsuario % i == 0) {
+				contador++;
+			}
+		}
+		if (contador == 2 || numUsuario == 1) {
+			System.out.println("El número " + numUsuario + " es primo");
+		} else {
+			System.out.println("El número " + numUsuario + " no es primo");
+		}
+
+		sc.close();
+
+	}
+
 	public static void main(String[] args) {
-		ejercicio21();
+		// ejercicio21();
+		ejercicio21Primos();
 
 	}
 
