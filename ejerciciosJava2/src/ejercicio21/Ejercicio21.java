91d71cd86eb787e8dcc761bf62c42241e9369c46
Dani <danielporras147@outlook.es>
2023-01-24 11:29:04 +0100

    Ejercicio 21

--- ejerciciosJava2/src/ejercicio21/Ejercicio21.java
+++ ejerciciosJava2/src/ejercicio21/Ejercicio21.java
@@ -1,11 +1,32 @@
+/**
+ * 21. Pide al usuario un número y dile los divisores de dicho número (resto da cero).
 
+ */
 
 package ejercicio21;
 
+import java.util.Scanner;
+
 public class Ejercicio21 {
 
+	public static void ejercicio21() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un número: ");
+		int numUsuario = sc.nextInt();
+		sc.nextLine();
+
+		for (int i = 1; i <= numUsuario; i++) {
+			if (numUsuario % i == 0) {
+				System.out.println("Los números divisores de " + numUsuario+ " son: " +i);
+			}
+		}
+
+		sc.close();
+
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ejercicio21();
 
 	}
 
