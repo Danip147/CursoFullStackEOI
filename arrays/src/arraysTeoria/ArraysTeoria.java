5f5b231b4028c53b33022d8cf220f63ac014eed7
Dani <danielporras147@outlook.es>
2023-01-25 08:46:39 +0100

    Arrays

---
+++ arrays/src/arraysTeoria/ArraysTeoria.java
@@ -0,0 +1,10 @@
+package arraysTeoria;
+
+public class ArraysTeoria {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ejerciciosArrays/src/ejerciciosArrays/EjerciciosArrays.java
@@ -0,0 +1,10 @@
+package ejerciciosArrays;
+
+public class EjerciciosArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
