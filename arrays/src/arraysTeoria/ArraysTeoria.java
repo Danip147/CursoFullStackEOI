02aa1509cf1c275f1ec0003845ba2f88a111f790
Dani <danielporras147@outlook.es>
2023-01-25 14:11:09 +0100

    añadiendo

--- arrays/src/arraysTeoria/ArraysTeoria.java
+++ arrays/src/arraysTeoria/ArraysTeoria.java
@@ -1,5 +1,9 @@
 package arraysTeoria;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class ArraysTeoria {
 
 	public static void ejemploDeclaraciones() {
@@ -30,25 +34,168 @@ public class ArraysTeoria {
 			System.out.println("El array numeros en su posción " + i + " tiene el valor " + numeros[i]);
 		}
 
+		// bucle for desde atrás hacia delante
+		for (int i = numeros.length - 1; i > 0; i--) {
+			System.out.println("El array numeros en su posción " + i + " tiene el valor " + numeros[i]);
+		}
+
 		// con un bucle for-each. No podemos saber la poscion
 		for (int numero : numeros) {
-			System.out.println(numero + 10);
+			System.out.println(numero);
 		}
-		
-		// bucle for-each con poscion . Para hacer esto hago el bucle for 
+
+		// bucle for-each con poscion . Para hacer esto hago el bucle for
 		int i = 0;
 		for (int numero : numeros) {
 			System.out.println("El array numeros en su posción " + i + " tiene el valor " + numero);
 			i++;
 		}
+
+	}
+
+	public static void ejemploBusquedas1() {
+		String[] nombres = { "Juan", "Ana", "Pedro", "Eva", "Paco" };
+		boolean encontrado = false; // Nos indicará si hemos encontrado el valor
+		String buscar = "Pedro"; // Valor a buscar en el array
+		// La condición también incluye que no hayamos encontrado lo que buscamos
+		for (int i = 0; i < nombres.length && !encontrado; i++) {
+			if (nombres[i].equals(buscar)) {
+				encontrado = true;
+			}
+		}
+		if (encontrado) {
+			System.out.println("El nombre " + buscar + " está en el array");
+		} else {
+			System.out.println(buscar + " no encontrado...");
+		}
+
+	}
+
+	// Programación funcional
+	public static void ejemploBusquedaFuncional() {
+		String[] nombres = { "Juan", "Ana", "Pedro", "Eva", "Paco" };
+		String buscar = "Pedro";
+		if (Arrays.stream(nombres).anyMatch(n -> n.equals(buscar))) { // con lambdas
+			System.out.println("El nombre " + buscar + " está en el array");
+		} else {
+			System.out.println(buscar + " no encontrado...");
+		}
+
+	}
+
+	public static void ejemploRedimension() {
+		String[] nombres = { "Juan", "Ana", "Pedro", "Eva", "Paco" };
+
+		nombres = Arrays.copyOf(nombres, 10);
+		nombres[5] = "Marta";
+		System.out.println(nombres.length); // 10
+		System.out.println(String.join(", ", nombres)); // Juan, Ana, Pedro, Eva, Paco, Marta, null, null, null, null
+	}
+
+	public static void ejemploAumentarUnoArray() {
+		Scanner sc = new Scanner(System.in);
+		String[] alumnos = { "Juan", "Ana", "Pedro", "Eva", "Paco" };
+		System.out.println("Introduzca en nuevo alumno: ");
+		String nombreAlumno = sc.nextLine();
+		alumnos = Arrays.copyOf(alumnos, alumnos.length + 1); // le da un tamaña de una unidad más al array
+
+		alumnos[alumnos.length - 1] = nombreAlumno; // Mete el nuevo dato en la ultima posicion
+
+		for (String alumno : alumnos) { // con for-each
+			System.out.println(alumno);
+		}
+
+		// Programacion funcional
+		Arrays.stream(alumnos).forEach(e -> System.out.println(e)); // e de elemento
+		sc.close();
+
+	}
+
+	// Eliminar por el final
+	public static void ejemploEliminarElementoArray() {
+		String[] alumnos = { "Juan", "Ana", "Pedro", "Eva", "Paco" };
+		alumnos = Arrays.copyOf(alumnos, alumnos.length - 1);
+		for (String alumno : alumnos) { // con for-each
+			System.out.println(alumno);
+		}
+
+	}
+
+	public static void ejemploOrdenarArray() {
+		// ordena lexicamente (primero las mayusculas...)
+		String[] alumnos = { "Juan", "Ana", "Pedro", "Eva", "Paco" };
+		Arrays.sort(alumnos);
+		System.out.println("Imprimir con for-each");
+		for (String alumno : alumnos) { // con for-each accedes a cada elemento
+			System.out.println(alumno);
+		}
+
+		System.out.println("Imprimir con toString");
+		System.out.println(Arrays.toString(alumnos)); // te muestra el contenido del array SOLO PARA VER
+
+		// ordenando números
+		int[] numeros = { 11, 2, 67, 4, 20, 45 };
+		Arrays.sort(numeros);
+		for (int numero : numeros) {
+			System.out.println(numero);
+		}
+	}
+
+	public static void ejemploOrdenarInverso() {
+		String[] alumnos = { "Juan", "Ana", "Pedro", "Eva", "Paco" };
+		Arrays.sort(alumnos); // orden menor a mayor
+		for (String alumno : alumnos) {
+			System.out.println(alumno);
+		}
+		// orden de mayor a menor
+		Collections.reverse(Arrays.asList(alumnos));
+		for (String alumno : alumnos) {
+			System.out.println(alumno);
+		}
+
+		// ordenando números
+		Integer[] numeros = { 11, 2, 67, 4, 20, 45 }; // Integer es la clase Wrapper de int, Boxing
+		Arrays.sort(numeros);
+		for (int numero : numeros) {
+			System.out.println(numero);
+		}
+
+		// orden de mayor a menor
+		Collections.reverse(Arrays.asList(numeros));
+		for (int numero : numeros) {
+			System.out.println(numero);
+		}
 		
 
 	}
 
+	public static void clasesEnvolventes() { // Wrappers 
+		// int -> Integer
+		Integer numero;
+		//double -> Double
+		Double decimal;
+		//float -> Float
+		Float decimal2;
+		//char -> Character
+		Character letra;
+		// boolean -> Boolean
+		Boolean booblean;
+		// String ya es una calse no tiene Wrapper
+		String cadena = "hola";
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		// ejemploDeclaraciones();
-		ejemploRecorrido();
-
+		// ejemploRecorrido();
+		// ejemploBusquedas1();
+		// ejemploBusquedaFuncional();
+		// ejemploRedimension();
+		// ejemploAumentarUnoArray();
+		// ejemploEliminarElementoArray();
+		// ejemploOrdenarArray();
+		ejemploOrdenarInverso();
 	}
 
 }
