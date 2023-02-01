1e93f95acd90ab528923a2e6165e737017252f89
Dani <danielporras147@outlook.es>
2023-02-01 20:22:40 +0100

    Ejercicio 12

---
+++ ejerciciosFunciones/src/ejercicio12/Ejercicio12.java
@@ -0,0 +1,31 @@
+/**
+ * 12. Crea una función que reciba un array de cadenas y te lo devuelva ordenado
+ * alfabéticamente de la ‘z’ a la ‘a’.
+ */
+
+package ejercicio12;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Ejercicio12 {
+	
+	public static String ordenaCadena(String... cadenas) {
+		Arrays.sort(cadenas); // lo ordeno
+		
+		Collections.reverse(Arrays.asList(cadenas)); // lo invierto
+		
+		 for (String cadena : cadenas) { // lo imprimo
+			System.out.println(cadena);
+		}
+		return null;
+		
+		
+	}
+
+	public static void main(String[] args) {
+		ordenaCadena("Dani", "Natalia", "Zoca", "Jaime");
+
+	}
+
+}
