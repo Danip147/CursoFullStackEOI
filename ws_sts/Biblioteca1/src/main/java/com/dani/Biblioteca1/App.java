ec7886b8c120b489764d2bf716233deb319b0e18
Dani <danielporras147@outlook.es>
2023-03-15 14:20:30 +0100

    añadiendo hibernate

---
+++ ws_sts/Biblioteca1/src/main/java/com/dani/Biblioteca1/App.java
@@ -0,0 +1,19 @@
+package com.dani.Biblioteca1;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+       Session session = sessionFactory.openSession();
+       if(session!=null) {
+    	   System.out.println("Sesion abierta correctamente");
+       }else {
+    	   System.out.println("Fallo al abrir la sesion");
+       }
+    }
+}
