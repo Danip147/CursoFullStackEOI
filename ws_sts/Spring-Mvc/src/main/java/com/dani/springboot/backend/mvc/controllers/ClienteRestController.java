f32129b97336fc75a3e6ef5c518195527029a642
Dani <danielporras147@outlook.es>
2023-03-22 12:29:04 +0100

    .

--- ws_sts/Spring-Mvc/src/main/java/com/dani/springboot/backend/mvc/controllers/ClienteRestController.java
+++ ws_sts/Spring-Mvc/src/main/java/com/dani/springboot/backend/mvc/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.dani.springboot.backend.mvc.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dani.springboot.backend.mvc.models.entity.Cliente;
 import com.dani.springboot.backend.mvc.models.services.IclienteService;
 
+@CrossOrigin(origins= {"*"}) // TODAS LAS APLICACIONES PUEDEN ACCEDER A LOS SERVICIOS !!IMPORTANTE!!
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
